c5c1add787b8e308514fcdfa1daac5ecb7ca2c95
Ethan Irvine <edog.irvine@gmail.com>
2019-11-25 16:14:22 -0700

    Created the prompts for the user to customize their passwords

---
+++ script.js
@@ -0,0 +1,96 @@
+// // function to generate our random password
+// function generate(){
+// // list of characters (Captial Letters, Lowercase Letters, numbers, special characters)
+//  let capLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+//  let lowLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+//  let numbers = ['1','2','3','4','5','6','7','8','9','0']
+//  let special = ['!','@','#','$','%','^','&','*','(',')','_','-','=']  
+
+//  // the following is going to allow the user to "cutomize" their password
+//  let passLength = prompt('How long would you like your password be? (must be between 7 & 129)') 
+//      if ((passLength <=7) || (passLength >= 129)) {
+//          alert('you chose ' + passLength)
+//      } else if ((passLength >= 7) || (passLength <= 129)) {
+//          alert ('you have to choose between 7 and 129')
+//      }
+    
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const passLength = prompt('How long would you like your password to be? (must be between 7 & 129)') 
+// if ((passLength >=7) || (passLength <= 129)) {
+//     alert('you chose ' + passLength)};
+
+  
+
+// let values = 'ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+';
+
+// let password = '';
+
+
+// for(var i = 0; i<= passLength; i++){
+//    password == password +values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+// }
+
+
+
+
+
+
+
+
+
+
+//  // list of characters (Captial Letters, Lowercase Letters, numbers, special characters)
+//  const capLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+//  const lowLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+//  const numbers = ['1','2','3','4','5','6','7','8','9','0']
+//  const specialChar = ['!','@','#','$','%','^','&','*','(',')','_','-','=']  
+
+//  // the following is going to allow the user to "cutomize" their password
+//  let passLength = prompt('How long would you like your password be? (must be between 7 & 129)') 
+//      if ((passLength <=7) || (passLength >= 129)) {
+//          alert('you chose ' + passLength)
+//      } else if ((passLength >= 7) || (passLength <= 129)) {
+//          alert ('you have to choose between 7 and 129')
+//      }
+    
